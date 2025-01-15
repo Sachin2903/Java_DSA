7e18b412b07b97cc817f9a4b7db599d13b5e3d06
sachin <sachinchaturvedi36952@gmail.com>
2025-01-16 00:11:04 +0530

    hashing

--- JavaBasic.java
+++ JavaBasic.java
@@ -3,21 +3,40 @@ import java.util.PriorityQueue;
 
 public class JavaBasic {
 
+    static class Row implements Comparable<Row>{
+        int soldiers;
+        int idx;
+        public Row(int s,int i){
+            this.soldiers=s;
+            this.idx=i;
+        }
+        @Override
+        public int compareTo(JavaBasic.Row arg0) {
+           if(this.soldiers==arg0.soldiers){
+            return this.idx-arg0.idx;
+           }else{
+            return this.soldiers-arg0.soldiers;
+           }
+        }
+    }
+
     public static void main(String[] args) {
-        int ropes[] = { 2, 3, 3, 4, 6 };
-        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        int arr[][]={{1,0,0,0},{1,1,1,1},{1,0,0,0},{1,0,0,0}};
 
-        for(int i=0;i<ropes.length;i++){
-            pq.add(ropes[i]);
+        int k=2;
+
+        PriorityQueue<Row> pq=new PriorityQueue<>();
+
+        for(int i=0;i<arr.length;i++){
+            int count=0;
+            for(int j=0;j<arr[0].length;j++){
+                count+=arr[i][j]==1?1:0;
+            }
+            pq.add(new Row(count,i));
         }
 
-        int cost=0;
-        while(pq.size()>1){
-            int min=pq.remove();
-            int min2=pq.remove();
-            cost+=min+min2;
-            pq.add(min+min2);
+        for(int i=0;i<k;i++){
+            System.out.println("R"+pq.remove().idx);
         }
-        System.out.println("cost of connecting n ropes = "+cost);
     }
 }
--- javaPart3.md
+++ javaPart3.md
@@ -1304,6 +1304,66 @@ m=4,n=4,k=2  ans=row0 & row2
 1 0 0 0
 1 0 0 0
 
+```java
+    static class Row implements Comparable<Row>{
+        int soldiers;
+        int idx;
+        public Row(int s,int i){
+            this.soldiers=s;
+            this.idx=i;
+        }
+        @Override
+        public int compareTo(JavaBasic.Row arg0) {
+           if(this.soldiers==arg0.soldiers){
+            return this.idx-arg0.idx;
+           }else{
+            return this.soldiers-arg0.soldiers;
+           }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[][]={{1,0,0,0},{1,1,1,1},{1,0,0,0},{1,0,0,0}};
+
+        int k=2;
+
+        PriorityQueue<Row> pq=new PriorityQueue<>();
+
+        for(int i=0;i<arr.length;i++){
+            int count=0;
+            for(int j=0;j<arr[0].length;j++){
+                count+=arr[i][j]==1?1:0;
+            }
+            pq.add(new Row(count,i));
+        }
+
+        for(int i=0;i<k;i++){
+            System.out.println("R"+pq.remove().idx);
+        }
+    }
+```
+
+## SLiding Window Maximum O(nlogk)
+
+maximum of all subarrrays of size k
+1,2,3,4,5,6,7,8,9,10  k=3
+
+ans=3,4,5,6,7,8,9,10
+
+2.
+1,3,-1,-3,5,3,6,7 k=3
+ans=3,3,5,5,6,7 
+
+
+add k number in PQ
+check the peek index should be from range i-k PQ.peek()
+add another in PQ
+
+
+# Hashing 
+
+
+
 
 
  
